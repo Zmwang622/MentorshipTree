0421361c9f2ec5f46a076f9ac51f2d833ce2ceb5
Zhengming Wang <zmwang622@gmail.com>
2020-11-02 00:34:38 -0500

    Add Gordon lu mentees

---
+++ src/js/mentees/gordon_lu.js
@@ -0,0 +1,45 @@
+var alex_lampe = {
+    parent: gordon_lu, 
+    text:{
+        name:"Alex Lampe",
+        desc: "CS 2024",
+        contact: {
+            val: "LinkedIn"
+        }
+    }
+},
+
+alex_wang = {
+    parent: gordon_lu, 
+    text:{
+        name:"Alex Wang",
+        desc: "IS 2024",
+        contact: {
+            val: "LinkedIn"
+        }
+    }
+},
+
+henry_powers = {
+    parent: gordon_lu, 
+    text:{
+        name:"Henry Powers",
+        desc: "CS 2024",
+        contact: {
+            val: "LinkedIn"
+        }
+    }
+},
+
+jamir_grier = {
+    parent: gordon_lu, 
+    text:{
+        name:"Jamir Grier",
+        desc: "CS 2024",
+        contact: {
+            val: "LinkedIn",
+            href: "https://www.linkedin.com/in/jamir-grier-594518182/",
+            target: "_blank"
+        }
+    }
+}; 
--- src/js/mentees/psuedo_root_2019.js
+++ src/js/mentees/psuedo_root_2019.js
@@ -9,4 +9,17 @@ var justin_kramer = {
             target:"_blank"
         }
     }
+},
+
+gordon_lu = {
+    parent: pseudo_root_2019,
+    text: {
+        name: "Gordon Lu",
+        desc: "CS 2022 | CSC Business Manager 2020-Present",
+        contact: {
+            val: "LinkedIn",
+            href:"https://www.linkedin.com/in/gordon-lu-aa1008152/",
+            target:"_blank"
+        }
+    }
 };
